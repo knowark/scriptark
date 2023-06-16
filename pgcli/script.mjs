2a3e008f4bdaa56279d5eaf4bc1e6ae8c74418d5
Esteban Echeverry <eecheverry@knowark.com>
2023-06-16 21:53:03 +0200

    Fix pgcli initial script.

--- pgcli/script.mjs
+++ pgcli/script.mjs
@@ -5,7 +5,6 @@ import { exec as shell } from 'node:child_process'
 
 const exec = util.promisify(shell)
 
-const { stdout, stderr } = await exec(ls)
+const { stdout, stderr } = await exec('ls')
 
-console.log('stdout:::', stdout)
-console.log('stderr:::', stderr)
+console.log('stdout:::\n', stdout)
