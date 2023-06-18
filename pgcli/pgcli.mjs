8b4c9915345b014ac99da81ff0ad71ff68e06f17
Esteban Echeverry <eecheverry@knowark.com>
2023-06-18 12:54:33 +0200

    Configure pgcli.

--- pgcli/pgcli.mjs
+++ pgcli/pgcli.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 import { spawn } from 'node:child_process'
+import { readFileSync, writeFileSync } from 'node:fs'
 
 const command = async (child) => {
   for await (const chunk of child.stdout) console.log(`${chunk}`)
@@ -16,5 +17,12 @@ await command(spawn('apt-get', ['install', 'pgcli', '-y']))
 console.log('\nInstalling PSPG...\n')
 
 await command(spawn('apt-get', ['install', 'pspg', '-y']))
+
+console.log('\nUpdating PGCLI configuration...\n')
+
+const file = './config/pgcli/config'
+const content = readFileSync(file).replace(
+  /# pager = less -SRXF/g, 'pager = pspg -i -s 5')
+writeFileSync(file, content)
 
 console.log('PGCLI installation and configuration has finished.\n')
